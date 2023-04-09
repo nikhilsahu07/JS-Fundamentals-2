0b52f886b6d400f7a260761afe4c5544e41dc533
Nikhil Sahu <heynikhil07@gmail.com>
2023-04-09 13:33:36 +0530

    Learned strict mode and it's benifits

---
+++ script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+//Activating strict mode
+
+//strict mode helps to find errors easily and prevent us from making further big bug
+//it must be in the first line of the js file otherwise it won't work
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest){
+//     hasDriverLicense = true; //js console didn't warn me about this without strict mode
+//     console.log("Allowed to drive..!");
+// } else {
+//     hasDriverLicense = false;
+// }
+
+// if (hasDriversLicense){
+//     console.log('I can drive..!');
+// }
+
+// let interface = 'qwerty';  //strict mode save you from illigal commands before they become bug
+// console.log(interface);
+// const private = 'home';
+
+
